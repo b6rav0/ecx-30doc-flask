eae8d5d07d03267f1d4189a1bf0bc49ff6197b91
bravo <adevcalledbravo@gmail.com>
2022-04-06 21:42:19 +0100

    day 2, LFG

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home/<name>')
-def home(name):
-    return render_template('index.html', name=name.capitalize())
+@app.route('/home/<username>')
+def home(username):
+    return render_template('index.html', username=username)
 
 
 if __name__ == "__main__":
