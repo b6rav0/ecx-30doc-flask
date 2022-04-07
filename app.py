54c9f66cfc41358811150e532b2841532ee990c5
bravo <adevcalledbravo@gmail.com>
2022-04-07 20:43:51 +0100

    day3

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Initializing the database
+db = SQLAlchemy(app)
+
+# Creating Database Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    date_registered = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 @app.route('/home/<username>')
 def home(username):
